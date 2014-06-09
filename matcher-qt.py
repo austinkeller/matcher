ed5a3eea50d2d35b701567bf2305a8ae4ce55620
Austin Keller <austin474@gmail.com>
2014-06-09 02:35:18 -0700

    Fixed GUI layout to align in grid. Connected GUI to matcher function.

--- matcher-qt.py
+++ matcher-qt.py
@@ -2,15 +2,25 @@
 
 import sys
 from PyQt4 import QtGui
+from PyQt4 import QtCore
 import matcher
 
 app = QtGui.QApplication(sys.argv)
 mainLayout = QtGui.QVBoxLayout()
-classifiersLayout = QtGui.QHBoxLayout()
+fileOutputLayout = QtGui.QHBoxLayout()
+classifiersLayout = QtGui.QGridLayout()
 dialog = QtGui.QDialog()
 dialog.setLayout(mainLayout)
 dialog.setWindowTitle("Matcher")
 button = QtGui.QPushButton("Go")
+
+# Select file for output
+fileOutputLayout.addWidget(QtGui.QLabel("Select Output File: "))
+fileOutputText = QtGui.QLineEdit()
+fileOutputLayout.addWidget(fileOutputText)
+fileOutputButton = QtGui.QPushButton("...")
+fileOutputLayout.addWidget(fileOutputButton)
+fileOutputText.setDisabled(True) # TODO: implement output file selection
 
 # Classifiers 
 class Classifier:
@@ -20,36 +30,56 @@ class Classifier:
         self.lineEdit = QtGui.QLineEdit()
         self.checkBox.setChecked(True) # checkbox defaults to on
 
-
-labelLayout = QtGui.QVBoxLayout()
-checkBoxLayout = QtGui.QVBoxLayout()
-errorValueLayout = QtGui.QVBoxLayout()
-errorTypeLayout = QtGui.QVBoxLayout()
-
-classifiersLayout.addLayout(labelLayout)
-classifiersLayout.addLayout(checkBoxLayout)
-classifiersLayout.addLayout(errorValueLayout)
-classifiersLayout.addLayout(errorTypeLayout)
-labelLayout.addWidget(QtGui.QLabel("Classifier"))
-checkBoxLayout.addWidget(QtGui.QLabel("Use?"))
-errorValueLayout.addWidget(QtGui.QLabel("Error Value"))
-errorTypeLayout.addWidget(QtGui.QLabel("Error Type"))
+labelLabel = QtGui.QLabel("Classifier")
+checkBoxLabel = QtGui.QLabel("Use?")
+errorValueLabel = QtGui.QLabel("Error Value")
+errorTypeLabel = QtGui.QLabel("Error Type")
+classifiersLayout.addWidget(labelLabel,0,0)
+classifiersLayout.addWidget(checkBoxLabel,0,1)
+classifiersLayout.addWidget(errorValueLabel,0,2)
+classifiersLayout.addWidget(errorTypeLabel,0,3)
+classifiersLayout.setAlignment(QtCore.Qt.AlignBottom)
 
 classifiers = {}
 
-for key in matcher.SORT_ORDER:
+for i,key in enumerate(matcher.SORT_ORDER):
     classifiers[key] = Classifier(key)
-    labelLayout.addWidget(classifiers[key].label)
-    checkBoxLayout.addWidget(classifiers[key].checkBox)
-    errorValueLayout.addWidget(classifiers[key].lineEdit)
+    classifiersLayout.addWidget(classifiers[key].label, i+1, 0)
+    classifiersLayout.addWidget(classifiers[key].checkBox, i+1, 1)
+    classifiersLayout.addWidget(classifiers[key].lineEdit, i+1, 2)
     try:
-        errorTypeLayout.addWidget(QtGui.QLabel(matcher.ERROR_TYPES[key]))
+        classifiers[key].lineEdit.setText(str(matcher.ERROR_RANGES[key]))
     except KeyError:
-        errorTypeLayout.addWidget(QtGui.QLabel(''))
-
+        classifiers[key].lineEdit.setDisabled(True)
+    try:
+        classifiersLayout.addWidget(QtGui.QLabel(matcher.ERROR_TYPES[key]), i+1, 3)
+    except KeyError:
+        classifiersLayout.addWidget(QtGui.QLabel(''), i+1, 3)
+mainLayout.addLayout(fileOutputLayout)
 mainLayout.addLayout(classifiersLayout)
 mainLayout.addWidget(button)
 
+@QtCore.pyqtSlot()
+def clicked_go():
+    sortOrder = []
+    errorRanges = {}
+    for key in classifiers:
+        if (classifiers[key].checkBox.isChecked()):
+            sortOrder.append(str(classifiers[key].label.text()))
+            try:
+                errorRanges[key] = float(classifiers[key].lineEdit.text())
+            except ValueError:
+                try:
+                    errorRanges[key] = matcher.ERROR_RANGES[key]
+                except KeyError:
+                    pass
+    print sortOrder
+    print errorRanges
+    print("Matching...")
+    matcher.matcher(sortOrder=sortOrder,errorRanges=errorRanges)
+    print("Done!")
+
+button.clicked.connect(clicked_go)
 
 dialog.show()
 sys.exit(app.exec_())
